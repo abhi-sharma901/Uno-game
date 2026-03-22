b331b87ac1aed9e2b4e1348886dd610a9b770825
Abhinav Sharma <abhinavsharma2001jk77@gmail.com>
2026-03-22 22:16:43 +0530

    adding the new spring applicatiion

---
+++ unogame/src/main/java/com/example/unogame/UnogameApplication.java
@@ -0,0 +1,13 @@
+package com.example.unogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnogameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnogameApplication.class, args);
+	}
+
+}
---
+++ unogame/src/test/java/com/example/unogame/UnogameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnogameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
